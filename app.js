d73282ddd33c572d05f7e51e81e1a1637e402ae0
trm621 <t.maher621@gmail.com>
2021-11-27 16:32:48 -0500

    added dynamically generated html

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 // fs.writeFile('index.html', pageHTML, err => {
 //     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!')
 // });
 
 const promptUser = () => {
@@ -53,11 +49,6 @@ const promptUser = () => {
                     return false;
                 }
             }
-        },
-        {
-            type: 'input',
-            name: 'about',
-            message: 'Provide some information about yourself.'
         }
     ]);
 };
@@ -141,5 +132,11 @@ return inquirer.prompt([
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData)
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+        if (err) throw new Error(err);
+
+        console.log('Page created! Check out index.html in this directory to see it!');
+        })
     });
