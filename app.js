18528ff5487522502005539ee6a139fb454ad967
trm621 <t.maher621@gmail.com>
2021-11-27 19:28:26 -0500

    added promise asynchronicity and cleaned up code

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page-template.js');
-
-// fs.writeFile('index.html', pageHTML, err => {
-//     if (err) throw err;
-// });
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -132,11 +128,18 @@ return inquirer.prompt([
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile('./index.html', pageHTML, err => {
-        if (err) throw new Error(err);
-
-        console.log('Page created! Check out index.html in this directory to see it!');
-        })
+       generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse)
+    })
+    .catch(err => {
+        console.log(err);
     });
